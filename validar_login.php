c5c50b08f633dc2f7cb4df44ad074780ffc4a46b
Johnzock <156722596+Johnzock@users.noreply.github.com>
2024-04-26 07:46:53 -0600

    validar_login.php

--- validar_login.php
+++ validar_login.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 $servidor = "localhost";
 $usuariodb = "root";
 $passdb = "";
@@ -6,17 +7,16 @@ $db = "galeria";
 
 $usuario = $_POST["user"];
 $contrasena = $_POST["pass"];
-$email = $_POST["email"];
 
 $conexion = mysqli_connect($servidor,$usuariodb,$passdb,$db);
-$consulta = "SELECT nombre, contrasena FROM usuarios WHERE nombre='$usuario' AND contrasena='$contrasena'";
-$resultado = misqli_query($conexion,$consulta);
+$consulta = "SELECT nombre,contrasena FROM usuarios WHERE nombre='$usuario' AND contrasena='$contrasena'";
+$resultado = mysqli_query($conexion, $consulta);
 
-//if(isset($usuarios[$usuario]) && $usuarios[$usuario] == $contrasena){
- if(mysqli_num_rows($resultado)>0){
+
+if(mysqli_num_rows($resultado)>0){
     $_SESSION['usuario'] = $usuario;
-    header("Location: upload.php")
+    header("Location: upload.php");
 }else{
-    echo "Error: Usuario y/o Contraseña incorrectos.";
+    echo "Error: Usuario y/o contrasena son incorrectos.";
 }
 ?>
